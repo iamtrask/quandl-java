70ebaea5daa90b521dfbc7069670e8ca4e986bcf
iamtrask <liamtrask@gmail.com>
2013-07-23 19:31:13 -0500

    changed document classpath to be api.java

---
+++ src/main/java/com/quandl/api/java/Examples.java
@@ -0,0 +1,37 @@
+package com.quandl.api.java;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Examples {
+
+    public final static void main(String[] args) throws Exception {
+
+        //open connection with key
+        QuandlConnection q = new QuandlConnection();
+
+        //open connection without key
+        QuandlConnection r = new QuandlConnection();
+
+        //get dataset from keyrange
+        QDataset data1 = q.getDataset("PRAGUESE/PX");
+
+        //get dataset between two sets of dates
+        QDataset data2 = q.getDatasetBetweenDates("PRAGUESE/PX","2012-01-01","2012-11-26");
+
+        //get dataset with custom parameters
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("trim_start","2012-09-30");
+        params.put("code","PX");
+        params.put("source_code","PRAGUESE");
+        QDataset data3 = q.getDatasetWithParams(params);
+
+        //get Dataset as array matrix
+        ArrayList<ArrayList<String>> data3Matrix = data3.getArrayMatrix();
+
+        //get Dataset as String Matrix
+        String[][] data3StringMatrix = data3.getStringMatrix();
+
+    }
+
+}
---
+++ src/main/java/com/quandl/api/java/QEntry.java
@@ -0,0 +1,44 @@
+package com.quandl.api.java;
+
+import org.json.simple.JSONArray;
+
+import java.util.ArrayList;
+
+/**
+ * Description of file content.
+ *
+ * @author atrask
+ *         7/21/13
+ */
+public class QEntry {
+
+    private String date;
+
+
+    private ArrayList<String> row;
+
+    public QEntry(JSONArray entry) {
+
+        row = new ArrayList<String>();
+
+        this.date = entry.get(0).toString();
+
+
+        for(Object eachValue : entry) {
+            row.add(eachValue.toString());
+        }
+
+
+    }
+
+    public ArrayList<String> getRow() {
+        return row;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+
+
+}
